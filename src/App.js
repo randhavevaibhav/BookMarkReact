123d16e6065125234f8acc8a3a45e641eb8704b0
Vaibhav Randhave <randhavevaibhav3@gmail.com>
2023-09-30 13:36:27 +0530

    added key for map

--- src/App.js
+++ src/App.js
@@ -1,188 +1,164 @@
 import "./App.css";
-import { useState,useRef } from "react";
-
+import { useState, useRef } from "react";
 
 function App() {
-  const savedData = localStorage.getItem('BookMarkData');
-const [BookMarkList, setBookMarkList] = useState(JSON.parse(savedData));
-const [currentBookMark, setCurrentBookMark] = useState();
-const [CurrentURL, setURL] = useState();
+  const savedData = localStorage.getItem("BookMarkData");
 
+  const savedDataList = JSON.parse(savedData);
 
+  const [BookMarkList, setBookMarkList] = useState(savedDataList);
 
-const inputBookMark = useRef(null);
-const inputURL = useRef(null);
-const  addBookMark= ()=>
-{
-  console.log(currentBookMark);
-  console.log("Calling addtask ---> " );
-  if((currentBookMark==="" || currentBookMark===undefined) || (CurrentURL==="" || CurrentURL===undefined))
-  {
-    alert("please give valid BookMark and URL !!");
-  }
-  else{
-    
-  
-  setBookMarkList([...BookMarkList,{bookMark:currentBookMark ,url:CurrentURL}]);
-  inputBookMark.current.value="";
-  setCurrentBookMark("");
-  inputURL.current.value="";
+  const [currentBookMark, setCurrentBookMark] = useState();
+  const [CurrentURL, setURL] = useState();
 
-  
+  const inputBookMark = useRef(null);
+  const inputURL = useRef(null);
+  const addBookMark = () => {
+    console.log(currentBookMark);
+    console.log("Calling addtask ---> ");
+    if (
+      currentBookMark === "" ||
+      currentBookMark === undefined ||
+      CurrentURL === "" ||
+      CurrentURL === undefined
+    ) {
+      alert("please give valid BookMark and URL !!");
+    } else {
+      setBookMarkList([
+        ...BookMarkList,
+        { bookMark: currentBookMark, url: CurrentURL },
+      ]);
+      inputBookMark.current.value = "";
+      setCurrentBookMark("");
+      inputURL.current.value = "";
+    }
+  };
 
-  }
-  
-}
+  const deleteBookMark = (bookMarkToDelete) => {
+    setBookMarkList(
+      BookMarkList.filter((bookMark) => {
+        return bookMark.bookMark !== bookMarkToDelete;
+      })
+    );
 
+    localStorage.setItem("BookMarkData", JSON.stringify(BookMarkList));
+  };
 
-
-
-const deleteBookMark = (bookMarkToDelete)=>
-{
-  
-    setBookMarkList(BookMarkList.filter((bookMark)=>{
-
-     return bookMark.bookMark!==bookMarkToDelete;
-
-    }))
-
-
-    localStorage.setItem('BookMarkData', JSON.stringify(BookMarkList));
-  
-    
-}
-
-
-
-
-
-
-const openTab = (tabNameString,URLString) =>
-{
-
-   
-  switch(tabNameString)
-      {
-        case 'PDFToWord':
+  const openTab = (tabNameString, URLString) => {
+    switch (tabNameString) {
+      case "PDFToWord":
         window.open("https://www.ilovepdf.com/pdf_to_word", "_self");
-          break;
-        case 'WordToPDF':
+        break;
+      case "WordToPDF":
         window.open("https://www.ilovepdf.com/word_to_pdf", "_self");
-          break;
-        case 'PDFCompressor':
+        break;
+      case "PDFCompressor":
         window.open("https://www.ilovepdf.com/compress_pdf", "_self");
-          break;
-          case 'IMGCompressor':
+        break;
+      case "IMGCompressor":
         window.open("https://www.iloveimg.com/compress-image", "_self");
-          break;
-          default:  alert(tabNameString+" is not a valid tab name !!");
-      }
-    
+        break;
+      default:
+        alert(tabNameString + " is not a valid tab name !!");
+    }
+  };
 
-}
-
-const redirect = (urlString) =>
-{
-  window.open(urlString, "_self");
-
-}
-
-
-
-
-
+  const redirect = (urlString) => {
+    window.open(urlString, "_self");
+  };
 
   return (
     <div className="App">
       <h1>Custom BookMarks</h1>
-     
-     <div className="default-buttons">
 
-            <button onClick={()=>{
+      <div className="default-buttons">
+        <button
+          onClick={() => {
+            openTab("PDFToWord");
+          }}
+        >
+          PDF To WORD
+        </button>
 
-        openTab("PDFToWord");
+        <button
+          onClick={() => {
+            openTab("WordToPDF");
+          }}
+        >
+          Word To PDF
+        </button>
 
-        }}>PDF To WORD</button>
-    
+        <button
+          onClick={() => {
+            openTab("PDFCompressor");
+          }}
+        >
+          PDF Compressor
+        </button>
 
-            <button onClick={()=>{
+        <button
+          onClick={() => {
+            openTab("IMGCompressor");
+          }}
+        >
+          IMG Compressor
+        </button>
+      </div>
 
-        openTab("WordToPDF");
-
-        }}>Word To PDF</button>
-
-          <button onClick={()=>{
-
-          openTab("PDFCompressor");
-
-          }}>PDF Compressor</button>
-
-          <button onClick={()=>{
-
-          openTab("IMGCompressor");
-
-          }}>IMG Compressor</button>
-
-
-     </div>
-
-
-
-      
       <div className="main-Frame">
-        <div className="bookmark-container" >
-          <input ref={inputBookMark} type="text" placeholder="BookMarkName...." onChange={(event)=>{setCurrentBookMark(event.target.value)}}></input>
-        
+        <div className="bookmark-container">
+          <input
+            ref={inputBookMark}
+            type="text"
+            placeholder="BookMarkName...."
+            onChange={(event) => {
+              setCurrentBookMark(event.target.value);
+            }}
+          ></input>
         </div>
         <div className="URL-container">
-            <input ref={inputURL} type="text" placeholder="URL...." onChange={(event)=>{setURL(event.target.value)}}></input>
-            <button onClick={addBookMark}>Add Custom Button</button>
-
-
+          <input
+            ref={inputURL}
+            type="text"
+            placeholder="URL...."
+            onChange={(event) => {
+              setURL(event.target.value);
+            }}
+          ></input>
+          <button onClick={addBookMark}>Add Custom Button</button>
         </div>
-        
-       
       </div>
-      <hr/>
+      <hr />
 
       <ul>
+        {BookMarkList.map((val, key) => {
+          return (
+            <div id="tasks" key={key + "2"}>
+              <button
+                className="customButton-name"
+                onClick={() => {
+                  redirect(val.url);
+                }}
+                key={key}
+              >
+                {" "}
+                {val.bookMark} {val.url}
+              </button>
 
-        {BookMarkList.map((val,key)=>
-        {
-            return ( <div id="tasks">
-              <button  className="customButton-name" onClick={()=>
-              {
-                redirect(val.url);
-              }} key={key}> {val.bookMark} {val.url}</button>
-              
-              <button onClick={()=>{deleteBookMark(val.bookMark)}}>X</button>
-
-          
-           
-              </div>
-              
-           
-              
-              );
-           
-                     
-             
-
+              <button
+                key={key + "0"}
+                onClick={() => {
+                  deleteBookMark(val.bookMark);
+                }}
+              >
+                X
+              </button>
+            </div>
+          );
         })}
-
-
-
-
-
       </ul>
 
-      
-      {localStorage.setItem('BookMarkData', JSON.stringify(BookMarkList))}
-      
-
-            
-           
-    
+      {localStorage.setItem("BookMarkData", JSON.stringify(BookMarkList))}
     </div>
   );
 }
